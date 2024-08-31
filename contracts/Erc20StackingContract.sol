da8969057f30cc7f70cab2f24853421dd4db10af
rozzy <rosekaremeri@gmail.com>
2024-08-31 07:41:52 +0300

    function stake token

--- contracts/Erc20StackingContract.sol
+++ contracts/Erc20StackingContract.sol
@@ -14,4 +14,59 @@ contract ERC20Staking {
         uint256 unlockTime;    // when the staked tokens can be withdrawn
         bool isWithdrawn;      // if the stake is withdrawn
     }
+
+     
+    mapping(address => Stake) public stakes; //store stakes by user address
+
+   
+    mapping(address => uint256) public rewardBalance;  //store calculated rewards
+
+    // Events for actions
+    event TokensStaked(address indexed user, uint256 amount, uint256 unlockTime);
+    event StakeWithdrawn(address indexed user, uint256 amount, uint256 reward);
+    event RewardRateUpdated(uint256 newRewardRate);
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+    modifier stakeLocked(address _user) {
+        require(block.timestamp >= stakes[_user].unlockTime, "Staking period not yet over");
+        _;
+    }
+    modifier nonZeroAddress(address _address) {
+        require(_address != address(0), "Zero address detected!");
+        _;
+    }
+
+    constructor(IERC20 _stakingToken, uint256 _initialRewardRate) {
+        owner = msg.sender;
+        stakingToken = _stakingToken;
+        rewardRatePerSecond = _initialRewardRate;
+    }
+
+    // Function to stake ERC20 tokens
+    function stakeTokens(uint256 _amount, uint256 _days) external nonZeroAddress(msg.sender) {
+        require(_amount > 0, "Cannot stake 0 tokens");
+        require(_days > 0, "Staking period must be at least 1 day");
+
+        Stake storage userStake = stakes[msg.sender];
+        require(userStake.amount == 0, "Existing stake found. Withdraw first.");
+
+        uint256 unlockTime = block.timestamp + (_days * 1 days);
+
+        // Transfer tokens from the user to the contract
+        require(stakingToken.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
+
+        userStake.amount = _amount;
+        userStake.startTime = block.timestamp;
+        userStake.unlockTime = unlockTime;
+        userStake.isWithdrawn = false;
+
+      
+
+        emit TokensStaked(msg.sender, _amount, unlockTime);
+    }
+
 }
