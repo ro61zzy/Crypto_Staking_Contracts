1468df89871ce7d5db0cafaadb9bfd18706dc46f
rozzy <rosekaremeri@gmail.com>
2024-08-31 07:39:39 +0300

    define global variables

--- contracts/Erc20StackingContract.sol
+++ contracts/Erc20StackingContract.sol
@@ -2,3 +2,11 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ERC20Staking {
+    IERC20 public stakingToken; // token to be staked
+    address public owner; // deployer/owner address
+    uint256 public rewardRatePerSecond; //adjustable by the owner
+
+    
+}
